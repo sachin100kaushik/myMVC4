216be11c84af582eb0f2e06428e783438c669fa3
Administrator100 <sachin100kaushik@outlook.com>
2017-02-12 22:54:11 -0800

    First comment

---
+++ myMVC/myMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myMVC.Models;
+
+namespace myMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        //public ActionResult getEmployeeDetails()
+        //{
+        //    Employee employee = new Employee() { 
+        //    EmployeeId=101,
+        //    Name="John",
+        //    Gender="Male",
+        //    City ="NY"
+        //    };
+        //    return View(employee);
+        //}
+
+        public ActionResult getEmployeeDetails(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+            return View(employee);
+        }
+    }
+}
---
+++ myMVC/myMVC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index(string id,string name)
+        {
+            return "Id=" + id+" Name="+name;
+        }
+
+        public ViewResult GetCountries()
+        {
+            ViewBag.Countries=new List<string>() {
+            "India",
+            "US",
+            "UK",
+            "Germany"
+            };
+
+            return View();
+        }
+        public ViewResult GetCountrieViewData()
+        {
+            ViewData["Countries"] = new List<string>() {
+            "India",
+            "US",
+            "UK",
+            "Germany",
+            "Spain"
+            };
+
+            return View();
+        }
+
+
+        public String GetMvcVersion()
+        {
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+
+        public string GetMethod()
+        {
+            return "Getmethod Invoked";
+        }
+
+    }
+}
