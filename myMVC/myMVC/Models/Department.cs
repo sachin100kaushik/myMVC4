3d13c49749dc7fc3abc513e0e6f507c18fb93ec8
Administrator100 <sachin100kaushik@outlook.com>
2017-02-14 01:15:52 -0800

    fourth commit

--- myMVC/myMVC/Models/Department.cs
+++ myMVC/myMVC/Models/Department.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace myMVC.Models
 {
+    [Table("tblDepartment")]
     public class Department
     {
+        [Column("Id")]
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Column("Name")]
+        public string Name { get; set; }      
         public List<Employee> employees { get; set; }
     }
 }
